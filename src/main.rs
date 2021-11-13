61834e99c48d264e3b947ab6f3efbfc8760de10f
Amanda Gouveia <amandasmithgouveia@gmail.com>
2021-11-13 18:36:56 -0500

    Initial setup.

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+extern crate rand;
+
+use rand::Rng;
+
+const BEAKER_SIZE: usize = 4;
+const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const FILLS: &str = "#$%^@&*!";
+
+fn mv (v: &mut Vec<[usize;BEAKER_SIZE]>, f: usize, t:usize) -> bool {
+    if v[f][BEAKER_SIZE-1] > 0 && v[t][0] == 0 {
+        
+        for i in 0..BEAKER_SIZE {
+            if v[f][i] > 0 {
+                for j in 0..BEAKER_SIZE {
+                    if v[t][j] > 0 {
+                        v[t][j-1] = v[f][i];
+                        v[f][i] = 0;
+                        return true;
+                    }
+                }
+                v[t][BEAKER_SIZE-1] = v[f][i];
+                v[f][i] = 0;
+                return true;
+            }
+        }
+        false
+    } else {
+        false
+    }
+}
+
+fn shuffle (v: &mut Vec<[usize;BEAKER_SIZE]>) {
+    let mut random_beakers: Vec<usize> = (0..v.len()).collect();
+    loop {
+        for _ in 0..100 {
+            rand::thread_rng().shuffle(&mut random_beakers);
+            mv(v, random_beakers[0], random_beakers[1]);
+        }
+        if !solved(&v){break;}
+    }
+}
+
+fn solved(v: &Vec<[usize;BEAKER_SIZE]>) -> bool {
+    for tube in v{
+        for i in 1..BEAKER_SIZE{
+            if tube[0] != tube[i] {
+                return false
+            }
+        }
+    }
+    true
+}
+fn test_out(v: &Vec<[usize;BEAKER_SIZE]>){
+    println!("{}",solved(&v));
+    println!("{:?}",v);
+}
+
+fn show_game(v: &Vec<[usize;BEAKER_SIZE]>){
+    let size = v.len();
+    let mut out = "".to_string();
+    for b in 0..size {
+        out += " | |";
+    }
+    println!("{}",out);
+    for row in 0..BEAKER_SIZE {
+        let mut out = "".to_string();
+        for b in 0..size {
+            out += " |";
+            if v[b][row] > 0{
+                //out += v[b][row].to_string().as_str();
+                out += FILLS[v[b][row]..v[b][row]+1].to_string().as_str();
+        
+            } else {
+                out += " ";
+            }
+            out += "|";
+        }
+        println!("{}", out);
+    }
+
+    out = "".to_string();
+    let mut out_numbers = "".to_string();
+    for b in 0..size {
+        out += " (_)";
+        out_numbers += "  ";
+        
+        out_numbers += ALPHABET[b..b+1].to_string().as_str();
+        out_numbers += " ";
+    }
+    println!("{}\n{}",out, out_numbers);
+    println!("Type q to quit or type AB to pour from A to B")
+}
+fn main() {
+    let difficulty = 4;
+    let mut bs: Vec<[usize; BEAKER_SIZE]> = vec![];
+    //= vec![[0; BEAKER_SIZE]];
+
+    for n in 0..difficulty + 1 {
+        bs.push([n; BEAKER_SIZE]);
+    }
+    shuffle(&mut bs);
+//    mv(&mut bs, 2, 0);
+    let result = loop {
+        show_game(&bs);
+        if solved(&bs) {
+            println!("WINNER!");
+            break true;
+        }
+        let mut action = String::new();
+        //show_game(&bs);
+        let b1 = std::io::stdin().read_line(&mut action).unwrap();
+        //println!("You said: {}", action);
+        if action == "q\n".to_string() {
+            println!("Quit");
+            break false;
+        } else {
+            if ALPHABET.contains(action[0..1].chars().next().unwrap()) {
+                let from_col = ALPHABET.chars().position(|c| c == action[0..1].chars().next().unwrap()).unwrap();
+                if ALPHABET.contains(action[1..2].chars().next().unwrap()) {
+                    let to_col = ALPHABET.chars().position(|c| c == action[1..2].chars().next().unwrap()).unwrap();
+                    mv(&mut bs, from_col, to_col);
+                }
+            }
+        }
+    }; 
+
+    println!("Goodbye.");
+}
