5366a5fdc0a35e712f2fb4082e12082e6c38d21f
Amanda Gouveia <amandasmithgouveia@gmail.com>
2021-11-14 22:00:03 -0500

    started ncurses setup

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,105 @@
 extern crate rand;
+extern crate ncurses;
 
 use rand::Rng;
+use ncurses::*;
+use std::cmp::min;
 
-use std::io;
-use termion::raw::IntoRawMode;
-use tui::Terminal;
-use tui::style::{Style, Color, Modifier};
-use tui::backend::TermionBackend;
-use tui::widgets::{Widget, Block, Borders, List, ListItem};
-use tui::layout::{Layout, Constraint, Direction};
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+const UI_PAIR: i16 = 2;
+
 
 const BEAKER_SIZE: usize = 4;
 const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const FILLS: &str = "^#$%@&!:><?()+=-~";
 
-fn mv (v: &mut Vec<[usize;BEAKER_SIZE]>, f: usize, t:usize) -> bool {
-    if v[f][BEAKER_SIZE-1] > 0 && v[t][0] == 0 {
-        
-        for i in 0..BEAKER_SIZE {
-            if v[f][i] > 0 {
-                for j in 0..BEAKER_SIZE {
-                    if v[t][j] > 0 {
-                        v[t][j-1] = v[f][i];
-                        v[f][i] = 0;
-                        return true;
+type Id = usize;
+
+struct Game {
+    difficulty: usize,
+    beakers: Vec<[usize; BEAKER_SIZE]>
+}
+impl Game {
+    fn mv (&mut self, f: usize, t:usize) -> bool {
+        if self.beakers[f][BEAKER_SIZE-1] > 0 && self.beakers[t][0] == 0 {
+            
+            for i in 0..BEAKER_SIZE {
+                if self.beakers[f][i] > 0 {
+                    for j in 0..BEAKER_SIZE {
+                        if self.beakers[t][j] > 0 {
+                            self.beakers[t][j-1] = self.beakers[f][i];
+                            self.beakers[f][i] = 0;
+                            return true;
+                        }
                     }
+                    self.beakers[t][BEAKER_SIZE-1] = self.beakers[f][i];
+                    self.beakers[f][i] = 0;
+                    return true;
                 }
-                v[t][BEAKER_SIZE-1] = v[f][i];
-                v[f][i] = 0;
-                return true;
             }
+            false
+        } else {
+            false
         }
-        false
-    } else {
-        false
     }
-}
 
-fn shuffle (v: &mut Vec<[usize;BEAKER_SIZE]>) {
-    let mut random_beakers: Vec<usize> = (0..v.len()).collect();
-    loop {
-        for _ in 0..200 {
-            rand::thread_rng().shuffle(&mut random_beakers);
-            mv(v, random_beakers[0], random_beakers[1]);
-        }
-        if !solved(&v){break;}
-    }
-}
-
-fn solved(v: &Vec<[usize;BEAKER_SIZE]>) -> bool {
-    for tube in v{
-        for i in 1..BEAKER_SIZE{
-            if tube[0] != tube[i] {
-                return false
+    fn shuffle (&mut self) {
+        let mut random_beakers: Vec<usize> = (0..self.beakers.len()).collect();
+        loop {
+            for _ in 0..200 {
+                rand::thread_rng().shuffle(&mut random_beakers);
+                &self.mv(random_beakers[0], random_beakers[1]);
             }
+            if !self.solved(){break;}
         }
     }
-    true
-}
-
-fn show_game(v: &Vec<[usize;BEAKER_SIZE]>){
-    let size = v.len();
-    let mut out: String;
     
-    println!("\n\n\n\nSORT THE LIQUIDS\n");
-
-    for row in 0..BEAKER_SIZE {
-        let mut out = "".to_string();
-        for b in 0..size {
-            out += " |";
-            if v[b][row] > 0{
-                out += FILLS[v[b][row]..v[b][row]+1].to_string().as_str();
-            } else {
-                out += " ";
+    fn solved(&self) -> bool {
+        for tube in &self.beakers {
+            for i in 1..BEAKER_SIZE{
+                if tube[0] != tube[i] {
+                    return false
+                }
             }
-            out += "|";
         }
-        println!("{}", out);
+        true
     }
-
-    out = "".to_string();
-    let mut out_numbers = "".to_string();
-    for b in 0..size {
-        out += " (_)";
-        out_numbers += "  ";
-        
-        out_numbers += ALPHABET[b..b+1].to_string().as_str();
-        out_numbers += " ";
-    }
-    println!("{}\n{}",out, out_numbers);
-    println!("\nType q to quit\nType AB to pour from A to B")
 }
-fn backupmain() {
+
+// fn show_game(v: &Vec<[usize;BEAKER_SIZE]>){
+//     let size = v.len();
+//     let mut out: String;
+    
+//     println!("\n\n\n\nSORT THE LIQUIDS\n");
+
+//     for row in 0..BEAKER_SIZE {
+//         let mut out = "".to_string();
+//         for b in 0..size {
+//             out += " |";
+//             if v[b][row] > 0{
+//                 out += FILLS[v[b][row]..v[b][row]+1].to_string().as_str();
+//             } else {
+//                 out += " ";
+//             }
+//             out += "|";
+//         }
+//         println!("{}", out);
+//     }
+
+//     out = "".to_string();
+//     let mut out_numbers = "".to_string();
+//     for b in 0..size {
+//         out += " (_)";
+//         out_numbers += "  ";
+        
+//         out_numbers += ALPHABET[b..b+1].to_string().as_str();
+//         out_numbers += " ";
+//     }
+//     println!("{}\n{}",out, out_numbers);
+//     println!("\nType q to quit\nType AB to pour from A to B")
+// }
+//fn backupmain() {
     // let difficulty = 8;
     // if difficulty > FILLS.len()-1 {
     //     println!("Max difficulty is {}. Goodbye.", FILLS.len()-1);
@@ -125,42 +133,70 @@ fn backupmain() {
     //         }
     //     }
     // }; 
+//}
+
+#[derive(Default)]
+struct Ui {
+    curr: Option<Id>,
+    selected: Option<Id>,
+    row: usize,
+    col: usize
 }
 
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.clear();
+impl Ui {
+    fn begin(&mut self, row: usize, col: usize){
+        self.row = row;
+        self.col = col;
+    }
     
-    terminal.draw(|f| {
-        
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
-                ].as_ref()
-            )
-            .split(f.size());
-        let block = Block::default()
-             .title("Block")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-        let block = Block::default()
-             .title("Block 2")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-        let items = [ListItem::new("Item 1"), ListItem::new("Item 2"), ListItem::new("Item 3")];
-        let mylist = List::new(items)
-            .block(Block::default().title("List").borders(Borders::ALL))
-            .style(Style::default().fg(Color::White))
-            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-            .highlight_symbol(">>");
-        f.render_widget(mylist, chunks[1]);
-    })?;
-    Ok(())
+    fn label(&mut self, text: &str, pair: i16) {
+        ncurses::mv(self.row as i32, self.col as i32);
+        attron(COLOR_PAIR(pair));
+        addstr(text);
+        attroff(COLOR_PAIR(pair));
+        self.row += 1;
+    }
+
+    fn end_list(&mut self) {
+        self.curr = None;
+    }
+    
+    fn end(&mut self) {
+
+    }
+}
+
+fn main() {
+    let mut selected = -1;
+
+    initscr();
+    noecho();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+    init_pair(UI_PAIR, COLOR_MAGENTA, COLOR_BLACK);
+
+    refresh();
+    
+    let mut quit = false;
+
+    let mut ui = Ui::default();
+
+    while !quit {
+        erase();
+        ui.begin(0,0);
+        {
+            ui.label("Sort the Colors", UI_PAIR);
+            
+        }
+        refresh();
+        let key = getch();
+        match key as u8 as char {
+            'q' => quit = true,
+            _ => ()
+        }
+    }
+
+    endwin();
 }
